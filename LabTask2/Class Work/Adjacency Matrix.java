2ca5c406e94fce560b2b9d5b79b4e01ff85c6700
Atik Jawad <85114480+AtikJawad@users.noreply.github.com>
2025-07-22 19:10:20 +0600

    Update Adjacency Matrix.java

--- LabTask2/Class Work/Adjacency Matrix.java
+++ LabTask2/Class Work/Adjacency Matrix.java
@@ -1,44 +1,92 @@
 import java.util.Scanner;
 
-public class GraphAdjMatrix {
-private int[][] adjMatrix;
-private int numVertices;
+class Adjacent {
+    private int[][] adjMatrix;
+    private int numVertices;
 
-// Constructor
-public GraphAdjMatrix(int numVertices) {
-this.numVertices = numVertices;
-adjMatrix = new int[numVertices][numVertices];
+    // Constructor
+    public Adjacent(int numVertices) {
+        this.numVertices = numVertices;
+        adjMatrix = new int[numVertices][numVertices]; // Matrix is initialized to 0 by default in Java
+
+        
+    }
+
+    // Unweighted Undirected
+    public void addEdge1(int u, int v) {
+        if (u >= 0 && u < numVertices && v >= 0 && v < numVertices) {
+            adjMatrix[u][v] = 1;
+            adjMatrix[v][u] = 1;
+        }
+    }
+
+    // Weighted Directed
+    public void addEdge2(int u, int v, int weight) {
+        if (u >= 0 && u < numVertices && v >= 0 && v < numVertices) {
+            adjMatrix[u][v] = weight;
+        }
+    }
+
+    // Weighted Undirected
+    public void addEdge3(int u, int v, int weight) {
+        if (u >= 0 && u < numVertices && v >= 0 && v < numVertices) {
+            adjMatrix[u][v] = weight;
+            adjMatrix[v][u] = weight;
+        }
+    }
+
+    public void show() {
+        System.out.println("Adjacency Matrix:");
+        for (int i = 0; i < numVertices; i++) {
+            for (int j = 0; j < numVertices; j++) {
+                System.out.print(adjMatrix[i][j] + "\t");
+            }
+            System.out.println();
+        }
+    }
 }
 
-// Add edge
-public void addEdge(int i, int j) {
-adjMatrix[i][j] = 1;
-adjMatrix[j][i] = 1;
-}
+public class Main {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
 
-// Print matrix
-public void printMatrix() {
-System.out.println(&quot;Adjacency Matrix:&quot;);
-for (int i = 0; i &lt; numVertices; i++) {
-for (int j = 0; j &lt; numVertices; j++) {
-System.out.print(adjMatrix[i][j] + &quot; &quot;);
-}
-System.out.println();
-}
-}
+        System.out.print("Press '1' for Unweighted Undirected, '2' for Weighted Directed, '3' for Weighted Undirected: ");
+        String option = input.nextLine();
 
-// Main method
-public static void main(String[] args) {
-GraphAdjMatrix graph = new GraphAdjMatrix(5);
+        Adjacent graph = new Adjacent(5);
 
-graph.addEdge(0, 1);
-graph.addEdge(0, 4);
-graph.addEdge(1, 2);
-graph.addEdge(1, 3);
-graph.addEdge(1, 4);
-graph.addEdge(2, 3);
-graph.addEdge(3, 4);
+        switch (option) {
+            case "1":
+                graph.addEdge1(0, 1);
+                graph.addEdge1(0, 4);
+                graph.addEdge1(1, 2);
+                graph.addEdge1(1, 3);
+                graph.addEdge1(1, 4);
+                graph.addEdge1(2, 3);
+                graph.addEdge1(3, 4);
+                break;
 
-graph.printMatrix();
-}
+            case "2":
+                graph.addEdge2(0, 1, 2);
+                graph.addEdge2(0, 4, 3);
+                graph.addEdge2(1, 2, -3);
+                graph.addEdge2(1, 3, 4);
+                graph.addEdge2(1, 4, 5);
+                graph.addEdge2(2, 3, -5);
+                graph.addEdge2(3, 4, 6);
+                break;
+
+            case "3":
+                graph.addEdge3(0, 1, 3);
+                graph.addEdge3(0, 4, -10);
+                graph.addEdge3(1, 2, -5);
+                graph.addEdge3(1, 3, 10);
+                break;
+
+            default:
+                System.out.println("Invalid Option");
+                return;
+        }
+        graph.show();
+    }
 }
