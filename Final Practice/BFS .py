f91ea5d0859af2e477466d228fa5deaa62aab057
Atik Jawad <85114480+AtikJawad@users.noreply.github.com>
2025-12-10 02:07:13 +0600

    Create BFS .py

---
+++ Final Practice/BFS .py
@@ -0,0 +1,71 @@
+class vertex:
+    def __init__(self,name):
+        self.name=name
+        self.color = "white"
+        self.dist = 9999
+        self.parent = None
+
+def bfs(vertices,adj,source):
+    queue = []
+
+    s= vertices[source]
+    s.color = "grey"
+    s.dist = 0
+    s.parent= None
+
+    queue.append(s)
+
+    bfsOrder= []
+
+    print("Vertex   |  Parent   |  Distance   |  Color")
+    print("__________________________________")
+
+    while queue:
+        u = queue.pop(0)
+        u_index = vertices.index(u)
+
+        bfsOrder.append(u)
+
+        parent_name = u.parent.name if u.parent else "Null"
+        print(f"{u.name} | {parent_name} | {u.dist} | {u.color}")
+
+        for v_index in range(len(adj)):
+            if adj[u_index][v_index] == 1:
+                v= vertices[v_index]
+
+                if v.color == "white":
+                    v.color = "grey"
+                    v.dist = u.dist + 1
+                    v.parent = u
+                    queue.append(v)
+        u.color = "black"
+
+    print("bfs order: ")
+    for vertices in bfsOrder:
+        print(vertices.name, end="->")
+
+
+
+filename = "G://Algo lab//input files//BFSinput.txt"
+
+with open(filename, "r") as f:
+        lines = [line.strip() for line in f.readlines()]
+
+verticeList= lines[0].split()
+n_vert = len(verticeList)
+vertices = [vertex(name) for name in verticeList]
+
+adj = [[0 for col in range(n_vert)] for row in range(n_vert) ]
+
+edgelines = lines[1:]
+
+for edge in edgelines:
+        u,v = map(int, edge.split())
+        adj[u][v] = 1
+        adj[v][u] = 1
+
+bfs(vertices, adj, 0)
+
+
+
+
