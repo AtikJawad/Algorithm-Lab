f33d1e0f07a5cfc78f9e4b41acb39a228c86cfa5
Atik Jawad <85114480+AtikJawad@users.noreply.github.com>
2025-12-11 02:07:44 +0600

    Update DFS .py

--- Final Practice/DFS .py
+++ Final Practice/DFS .py
@@ -1,80 +1,54 @@
-from fileinput import filename
-
-
-class DFS:
+class dfs:
     def __init__(self,n):
-        self.n=n
-        self.color = ["white"] * n
-        self.parent = [-1] * n
-        self.beg_time = [0] * n
-        self.end_time = [0] * n
+        self.n = n
+        self.color = ["white"]*n
+        self.parent = [-1]*n
+        self.d=[0]*n
+        self.f= [0]*n
         self.time = 0
-        self.DfsOrder =[]
+        self.dfs_order = []
+        self.topological_sort = []
 
-    def doDFS(self,adj):
-        self.adj = adj
+    def DFS(self,adj):
+        self.adj= adj
+
         for u in range(self.n):
             self.color[u] = "white"
             self.parent[u] = -1
-            self.beg_time[u] = 0
-            self.end_time[u] = 0 #not in algo
+            self.d[u] = 0
+            self.f[u] = 0
         self.time = 0
-
-        print("Vertex   |  Color  |   Beg Time  |   End Time  |   Parent")
-
+        print("Vertex\tParent\tBeg_time\tend_time\tcolor")
         for u in range(self.n):
             if self.color[u] == "white":
-                self.DFS_VISIT(u)
-        print(f"DFS Order: {self.DfsOrder} ")
-    def DFS_VISIT(self,u):
+                self.DFS_visit(u)
+        print("DFS Order: ", self.dfs_order)
+        print("Topological sort: ", list(reversed(self.topological_sort)))
+
+    def DFS_visit(self,u):
         self.color[u] = "grey"
-        self.time+=1
-        self.beg_time[u] = self.time
+        self.time += 1
+        self.d[u] = self.time
 
-        self.DfsOrder.append(u)
-
-
+        self.dfs_order.append(u)
         for v in range(self.n):
             if self.adj[u][v] == 1 and self.color[v] == "white":
                 self.parent[v] = u
-                self.DFS_VISIT(v)
+                self.DFS_visit(v)
         self.color[u] = "black"
         self.time += 1
-        self.end_time[u] = self.time
-
-        print(f"{u}  {self.color[u]}   {self.beg_time[u]}    {self.end_time[u]}   {self.parent[u]}")
-
-filename="G:\\Algo lab\\input files\\DFS input.txt"
-with open(filename, "r") as f:
-    lines = [line.strip() for line in f.readlines()]
-
-n_vert = int(lines[0])
+        self.f[u] = self.time
+        self.topological_sort.append(u)
+        print(f"{u}\t{self.parent[u]}\t{self.d[u]}\t{self.f[u]}\t{self.color[u]}")
 
 
-adj = [[0 for col in range(n_vert)] for row in range(n_vert)]
+n= int(input("number of Vertx: "))
+adj=[]
 
-edgelines= lines[1:]
+for _ in range(n):
+    row_values = input(f"Input {_} adjacency row with space: ").strip()
+    row = list(map(int,row_values.split()))
+    adj.append(row)
 
-for edge in edgelines:
-    u,v = map(int, edge.split())
-    adj[u][v] = 1
-    adj [v][u] = 1
-
-dfs = DFS(n_vert)
-dfs.doDFS(adj)
-
-
-#adj = [
-    #     [0, 1, 1, 0, 0, 0, 0, 0],
-    #     [0, 0, 0, 0, 0, 0, 0, 1],
-    #     [0, 1, 0, 0, 0, 0, 0, 0],
-    #     [0, 0, 1, 0, 1, 0, 0, 0],
-    #     [0, 0, 0, 1, 0, 0, 0, 0],
-    #     [0, 0, 0, 1, 1, 0, 1, 0],
-    #     [0, 0, 1, 0, 0, 0, 0, 0],
-    #     [1, 0, 1, 1, 0, 0, 0, 0]
-    # ]
-    # 
-    # dfs = DFS(8)
-    # dfs.doDFS(adj)
-
+traverse = dfs(n)
+traverse.DFS(adj)
